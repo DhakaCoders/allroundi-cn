c17ca69a4d46fdbdad8c55da5d9ece21d2fb25a5
sabbir3109 <78914877+sabbir3109@users.noreply.github.com>
2021-02-27 21:05:10 +0600

    ...

--- themes/allround/assets/js/main.js
+++ themes/allround/assets/js/main.js
@@ -1,12 +1,20 @@
 (function($) {
 
-/*Google Map Style*/
-var CustomMapStyles  = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
-
-var windowWidth = $(window).width();
-$('.navbar-toggle').on('click', function(){
-	$('#mobile-nav').slideToggle(300);
-});
+$(".hamburger-btn").click(function(){
+    $(".xs-mobile-menu").slideToggle(350);
+    $(this).toggleClass("close-icon");
+  });
+  $('li.menu-item-has-children > a').on('click', function(e){
+    e.preventDefault();
+    $(this).parent().siblings().find('.sub-menu').slideUp(300);
+    $(this).parent().find('.sub-menu').slideToggle(300);
+    $(this).toggleClass('sub-menu-active');
+    $(this).parent().siblings().find('.sub-menu-active').removeClass('sub-menu-active');
+  });
+  // $('nav.main-nav > ul > li > a').on('click', function(e){
+  //   e.preventDefault();
+  //   $('li.menu-item-has-children > a').parents().siblings().find('.sub-menu').slideUp(300);
+  // });
 	
   
 //matchHeightCol
